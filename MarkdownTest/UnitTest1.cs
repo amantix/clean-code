f21ac41151f2425315ab35da039db213f812b6f5
Dragn1ll <abdulloev-2005@mail.ru>
2024-09-20 14:56:42 +0300

    Added tests

---
+++ MarkdownTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MarkdownTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
