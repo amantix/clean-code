becbc23dce64e4c7f4e070ba6f491754b7c4a04a
79196 <Gre4ka28@yandex.ru>
2024-09-24 13:28:10 +0300

    добавил классы

---
+++ Markdown/Markdown/MarkdownProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Markdown;
+
+public class MarkdownProcessor
+{
+    private MarkdownParser _parser;
+    private MarkdownRenderer _renderer;
+    public MarkdownProcessor()
+    {
+        _parser = new MarkdownParser();
+        _renderer = new MarkdownRenderer();
+    }
+    public string GetHtml(string markdownText)
+    {
+        var elements = _parser.Parse(markdownText);
+        var html = _renderer.Render(elements);
+        return html.ToString();
+    }  
+}
---
+++ Markdown/Markdown/MarkdownRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    public class MarkdownRenderer
+    {
+        public string Render(List<IMarkdownElement> elements)
+        {
+            var html = new StringBuilder();
+
+            foreach (var element in elements)
+            {
+                html.AppendLine(element.GetHtmlLine());
+            }
+
+            return html.ToString();
+        }
+    }
+}
